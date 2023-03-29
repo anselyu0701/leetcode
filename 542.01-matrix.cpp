ca7dd8138522e1dcb848e9413358b06deb6acb8b
anselyu0701 <st20359@gmail.com>
2023-03-29 21:57:55 +0800

    0329_2029

--- 542.01-matrix.cpp
+++ 542.01-matrix.cpp
@@ -10,7 +10,9 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
     {
         int counter1, counter2;
+        // define a 2d-vector to save the distance and initialize it with a greatest number (10000)
         vector<vector<int>> v(mat.size(), vector<int>(mat[0].size(), 10000));
+        // the first traversal: from the top left to down right
         for (counter1 = 0; counter1 < mat.size(); ++counter1)
         {
             for (counter2 = 0; counter2 < mat[0].size(); ++counter2)
@@ -32,6 +34,7 @@ public:
                 }
             }
         }
+        // the second traversal: from the down right to top left
         for (counter1 = mat.size()-1; counter1 >= 0; --counter1)
         {
             for (counter2 = (mat[0].size()-1); counter2 >= 0; --counter2)
