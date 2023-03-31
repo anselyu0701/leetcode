0d96d776cc12ea5707479403b6d647be268296a9
anselyu0701 <st20359@gmail.com>
2023-03-31 16:59:18 +0800

    0331_2023

--- 218.the-skyline-problem.cpp
+++ 218.the-skyline-problem.cpp
@@ -5,88 +5,52 @@
  */
 
 // @lc code=start
+//using Multiset
+// refer to https://leetcode.com/problems/the-skyline-problem/solutions/2094329/c-easiest-explanation-ever-guaranteed-beginner-friendly-detailed-o-nlogn/
 class Solution {
 public:
-    vector<vector<int>> ans;
-    int getMaxHeight(vector<vector<int>>& buildings, int pointer)
-    {
-        int height = buildings[pointer][2];
-        for (int index = 0; index < buildings.size(); index++)
-        {
-            if (index != pointer)
-            {
-                if (buildings[pointer][0] >= buildings[index][0] && buildings[pointer][0] <= buildings[index][1])
-                {
-                    // overlapping happened, find the max height
-                    height = max(buildings[pointer][2], buildings[index][2]);
-                    // head, head overlapping
-                    if (buildings[pointer][2] == buildings[index][2] && buildings[pointer][0] != buildings[index][0])
-                    {
-                        return -1;
-                    }
-                }
-                if (buildings[pointer][0] == buildings[index][1] && buildings[pointer][2] == buildings[index][2])
-                {
-                    // head, rear connected
-                    return -1;
-                }
-            }
-        }
-        if (height != buildings[pointer][2])
-        {
-            return -1; // be overlapped and samller than others
-        }
-        else
-        {
-            return height; // not be overlapped or bigger than others
-        }
-    }
-
-    int checkRear(vector<vector<int>>& buildings, int pointer)
-    {
-        int height = 0;
-        for (int index = 0; index < buildings.size(); index++)
-        {
-            if (index != pointer)
-            {
-                if (buildings[pointer][1] >= buildings[index][0] && buildings[pointer][1] <= buildings[index][1])
-                {
-                    if (buildings[index][2] >= buildings[pointer][2])
-                    {
-                        return -1;
-                    }
-                    else if (buildings[pointer][1] == buildings[index][1])
-                    {
-                        height = 0;
-                    }
-                    else
-                    {
-                        height = max(height, buildings[index][2]);
-                    }
-                }
-            }
-        }
-        return height; // not be overlapped
-    }
-
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        sort(buildings.begin(), buildings.end());
-        int height, rear;
-        for (int pointer = 0; pointer < buildings.size(); pointer++)
+        vector<pair<int, int>> heights;
+        vector<vector<int>> ans;
+
+        for (auto build : buildings)
         {
-            height = getMaxHeight(buildings, pointer);
-            if (height != -1)
+            heights.push_back({build[0], -build[2]}); // store the start point with -height
+            heights.push_back({build[1],  build[2]}); // store the end point with +height
+        }
+
+        sort(heights.begin(), heights.end());
+
+        // define a multiset that store the numbers in decreasing order
+        multiset<int, greater<int>> ms;
+        ms.insert(0);
+        int  lastMax = 0;
+
+        for (auto it : heights)
+        {
+            if (it.second < 0)
             {
-                ans.push_back({buildings[pointer][0], height});
+                // if height is negative(at head) then adding to the set
+                ms.insert(-it.second);
             }
-            rear = checkRear(buildings, pointer);            
-            if (rear != -1)
+            else
             {
-                ans.push_back({buildings[pointer][1], rear});
+                // if height is positive(at rear) then removing from the set
+               ms.erase(ms.find(it.second));
+            }
+
+            // find the biggest element in the set, then equal to curMax
+            int curMax = *(ms.begin());
+
+            // if the curMax != lastMax, that means it's a turning point
+            // it needs to be add to the ans vector 
+            if(curMax != lastMax)
+            {
+                ans.push_back({it.first, curMax});
+                lastMax = curMax;
             }
         }
         return ans;
     }
 };
 // @lc code=end
-
